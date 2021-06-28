6cb7594a556e5c9cc15f6686a7c174faa9d27cec
Mario Diaz <dev.mariodiaz@gmail.com>
2021-06-28 18:07:08 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import Todo from './components/Todo'
 import './App.css';
+import { FilterButton } from './components/FilterButton';
+import { Form } from './components/Form';
+import React, { useState } from 'react';
+import {nanoid} from 'nanoid';
 
-function App() {
+function App(props) {
+
+  const [tasks, setTask] = useState(props.tasks);
+
+  const taskList = props.tasks.map(task => (
+    <Todo 
+      id={ task.id } 
+      name={ task.name } 
+      completed={ task.completed } 
+      key={ task.id } 
+    />
+  ));
+
+  const addTask = (name) => {
+    const newTask = {
+      id: `todo-${ nanoid() }`,
+      name: name, 
+      completed: false
+    };
+
+    setTask([...tasks, newTask]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={ addTask } />
+      <div className="filters btn-group stack-exception">
+        <FilterButton name="All" pressed={ true } />
+        <FilterButton name="Active" pressed={ false } />
+        <FilterButton name="Completed" pressed={ false } />
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
     </div>
   );
 }
---
+++ src/components/FilterButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const FilterButton = (props) => {
+    return (
+        <button type="button" className="btn toggle-btn" aria-pressed={props.pressed}>
+          <span className="visually-hidden">Show </span>
+          <span>{ props.name }</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+    )
+}
+
+
+export default FilterButton;
